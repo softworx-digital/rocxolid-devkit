e403962cd0a7ac931ebfefe454d085d23e609cba
Peter Bolemant <peter@softworx.digital>
2020-12-20 13:35:53 +0000

    general update

--- resources/views/commander/command/form/default.blade.php
+++ resources/views/commander/command/form/default.blade.php
@@ -1,7 +1,7 @@
 {{ Form::open($component->getOptions()->toArray()) }}
     @if (!$component->getForm()->getErrors()->isEmpty())
         <div class="alert alert-danger alert-dismissible fade in" data-timeout-remove="2000" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true"><i class="fa fa-close"></i></span></button>
+            <button type="button" class="close" data-dismiss="alert"><span><i class="fa fa-close"></i></span></button>
             <ul>
             @foreach ($component->getForm()->getErrors()->all() as $error)
                 <li>{!! $error !!}</li>
