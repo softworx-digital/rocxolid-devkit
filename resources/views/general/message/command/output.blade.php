21533746ee1d571a36a7dd4ef9ac0e1c75ec0f28
Peter Bolemant <peter@softworx.digital>
2021-01-25 15:48:06 +0000

    general update

--- resources/views/general/message/command/output.blade.php
+++ resources/views/general/message/command/output.blade.php
@@ -1,5 +1,5 @@
 @if (isset($output[$command]))
-<pre id="{{ $tab->makeDomId($command, 'output') }}" class="margin-top-20">{{ $output[$command] }}</pre>
+<pre id="{{ $tab->getDomId($command, 'output') }}" class="margin-top-20">{{ $output[$command] }}</pre>
 @else
-<span id="{{ $tab->makeDomId($command, 'output') }}"></span>
+<span id="{{ $tab->getDomId($command, 'output') }}"></span>
 @endif
